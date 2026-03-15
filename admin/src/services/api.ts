32222c42109f46aeae2ad414a4f490c9416d5c9b
Jose <josemsilva1984@gmail.com>
2026-03-15 00:02:23 -0300

    api_ts

--- admin/src/services/api.ts
+++ admin/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-baseURL: "https://api.acaiecompanhia.com.br"
+baseURL: "https://api.acaiecompanhia.com.br/api"
 })
 
 api.interceptors.request.use((config) => {
