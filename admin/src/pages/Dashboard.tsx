f49ef0dd8405ed38428328ff339db69f06043dbe
Jose <josemsilva1984@gmail.com>
2026-03-22 16:09:07 -0300

    modif login

--- admin/src/pages/Dashboard.tsx
+++ admin/src/pages/Dashboard.tsx
@@ -1,9 +1,57 @@
-export default function Dashboard() {
+import { useEffect, useState } from "react"
+import api from "../services/api"
 
-  return (
-    <div style={{ padding: 20 }}>
+export default function Dashboard(){
+
+  const [produtos,setProdutos] = useState(0)
+  const [pedidos,setPedidos] = useState(0)
+
+  useEffect(()=>{
+
+    async function carregar(){
+
+      const p = await api.get("/produtos")
+      setProdutos(p.data.length)
+
+      const pe = await api.get("/pedidos")
+      setPedidos(pe.data.length)
+
+    }
+
+    carregar()
+
+  },[])
+
+  return(
+
+    <div style={{padding:40}}>
+
       <h1>Dashboard</h1>
+
+      <div style={{display:"flex",gap:20}}>
+
+        <div style={{
+          padding:20,
+          background:"#eee",
+          borderRadius:10
+        }}>
+          <h2>Produtos</h2>
+          <h3>{produtos}</h3>
+        </div>
+
+        <div style={{
+          padding:20,
+          background:"#eee",
+          borderRadius:10
+        }}>
+          <h2>Pedidos</h2>
+          <h3>{pedidos}</h3>
+        </div>
+
+      </div>
+
     </div>
+
   )
 
 }
