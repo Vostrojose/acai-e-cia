31bf279ab8d82018d3c111d7122071afdd2ba9c0
Jose <josemsilva1984@gmail.com>
2026-04-03 13:06:19 -0300

    adm aud

--- admin/src/pages/Cozinha.tsx
+++ admin/src/pages/Cozinha.tsx
@@ -128,7 +128,7 @@ export default function Cozinha() {
         style={{
           display: 'grid',
           gridTemplateColumns: '1fr 1fr 1fr',
-          gap: 60,
+          gap: 40,
         }}
       >
         <Coluna titulo="🆕 Novos Pedidos" pedidos={novos} />
