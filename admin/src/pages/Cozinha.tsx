3b771bda7ec1431ec91930a1e74f5ed7313ab6cc
Jose <josemsilva1984@gmail.com>
2026-04-03 12:52:27 -0300

    adm aud

--- admin/src/pages/Cozinha.tsx
+++ admin/src/pages/Cozinha.tsx
@@ -136,7 +136,7 @@ export default function Cozinha() {
       <div
         style={{
           display: 'grid',
-          gridTemplateColumns: '1fr 1fr 1fr',
+      gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
           gap: 20,
         }}
       >
