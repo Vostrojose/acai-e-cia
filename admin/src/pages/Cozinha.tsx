9e535f4b067bc6b825ce7e5721ef31472d48b3a2
Jose <josemsilva1984@gmail.com>
2026-04-03 15:35:13 -0300

    adm aud

--- admin/src/pages/Cozinha.tsx
+++ admin/src/pages/Cozinha.tsx
@@ -216,7 +216,7 @@ function CardStatus({ titulo, valor, cor }: any) {
 /* COLUNA */
 function Coluna({ titulo, pedidos }: any) {
   return (
-    <div style={{ background: '#8009bcc2', borderRadius: 10, padding: 20 }}>
+    <div style={{ background: '#672df0', borderRadius: 10, padding: 20 }}>
       <h2>{titulo}</h2>
       {pedidos.map((pedido: any) => (
         <PedidoCard key={pedido.id} pedido={pedido} />
