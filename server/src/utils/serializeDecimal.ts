1976b23966b500e03dd3a577da3f2d66624f9f3f
Jose <josemsilva1984@gmail.com>
2026-04-04 02:18:42 -0300

    fix: corrigindo controller pedido

--- server/src/utils/serializeDecimal.ts
+++ server/src/utils/serializeDecimal.ts
@@ -3,12 +3,18 @@ export function serializeDecimal(obj: any): any {
     return obj.map(serializeDecimal)
   }
 
+  // ✅ NÃO mexer em Date
+  if (obj instanceof Date) {
+    return obj.toISOString()
+  }
+
   if (obj !== null && typeof obj === 'object') {
     const newObj: any = {}
 
     for (const key in obj) {
       const value = obj[key]
 
+      // ✅ tratar Decimal
       if (
         value &&
         typeof value === 'object' &&
@@ -16,7 +22,12 @@ export function serializeDecimal(obj: any): any {
         value.constructor.name === 'Decimal'
       ) {
         newObj[key] = Number(value)
-      } else {
+      }
+      // ✅ tratar Date corretamente
+      else if (value instanceof Date) {
+        newObj[key] = value.toISOString()
+      }
+      else {
         newObj[key] = serializeDecimal(value)
       }
     }
