844b6406d1b5a8d7ec4f388906855fa9e585c99d
Jose <josemsilva1984@gmail.com>
2026-04-03 22:44:03 -0300

    fix: separando frontend e backend corretamente

--- web-client/src/main.tsx
+++ web-client/src/main.tsx
@@ -4,6 +4,15 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 
+// 🔥 REMOVE SERVICE WORKER (resolve cache)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    for (let registration of registrations) {
+      registration.unregister()
+    }
+  })
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
