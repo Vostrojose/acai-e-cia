58018125efe3096140819d402d8eadf21b8c1a62
Jose <josemsilva1984@gmail.com>
2026-04-18 16:09:18 -0300

    att versel jason

--- admin/src/pages/Produtos.tsx
+++ admin/src/pages/Produtos.tsx
@@ -171,7 +171,7 @@ export default function Produtos() {
 
 const page = {
   padding: 20,
-  background: "#f5f5f5",
+  background: "#6d10f9a2",
   minHeight: "100vh",
 };
 
@@ -195,7 +195,7 @@ const cardAçai = {
 };
 
 const cardContent = {
-  background: "#fff",
+  background: "#0b67f2",
   borderRadius: 10,
   padding: 15,
 };
