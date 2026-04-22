e136d133753274154fe6610e13ddd173321e531c
Jose <josemsilva1984@gmail.com>
2026-04-22 20:59:35 -0300

    fix: adiciona css acompanhamento

--- web-client/src/pages/Acompanhamento.tsx
+++ web-client/src/pages/Acompanhamento.tsx
@@ -3,7 +3,6 @@ import { io } from 'socket.io-client'
 import { useParams } from 'react-router-dom'
 import api from '../services/api'
 import { useCart } from '../context/CartContext'
-
 import '../assets/css/Acompanhamento.css'
 
 
