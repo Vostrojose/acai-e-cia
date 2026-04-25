b3c9e02373f783f3bfeb12a0bcc50d1e3a52eb08
Jose <josemsilva1984@gmail.com>
2026-04-25 01:08:51 -0300

    test: log versao nova

--- server/src/validators/pedido.schema.ts
+++ server/src/validators/pedido.schema.ts
@@ -3,14 +3,21 @@ import { z } from "zod";
 export const criarPedidoSchema = z.object({
   telefone: z.string().min(10),
   origem: z.enum(["QR_CODE", "APP", "ADMIN", "BALCAO"]),
-  
-  // 🔥 CORREÇÃO AQUI
+
   endereco: z.string().nullable().optional(),
 
   itens: z.array(
     z.object({
       produtoId: z.string().uuid(),
       quantidade: z.number().min(1),
+
+      // 🔥 CORREÇÃO AQUI (ESSENCIAL)
+      adicionais: z.array(
+        z.object({
+          nome: z.string(),
+          preco: z.number()
+        })
+      ).optional()
     })
   ),
 });
