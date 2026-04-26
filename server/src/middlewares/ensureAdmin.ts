2736c356fa6b24e970ff736e7dc685abcee6521f
Jose <josemsilva1984@gmail.com>
2026-04-26 14:08:39 -0300

    ajuste de segurança hash

--- server/src/middlewares/ensureAdmin.ts
+++ server/src/middlewares/ensureAdmin.ts
@@ -5,15 +5,17 @@ export function ensureAdmin(
   res: Response,
   next: NextFunction
 ) {
+  console.log("🔥 USER NO ensureAdmin:", req.user);
+
   if (!req.user) {
     return res.status(401).json({
       message: "Não autenticado"
     });
   }
 
-  if (req.user.role !== "admin") {
+  if (req.user.role !== "ADMIN") {
     return res.status(403).json({
-      message: "Acesso negado"
+      message: "Sem permissão"
     });
   }
 
