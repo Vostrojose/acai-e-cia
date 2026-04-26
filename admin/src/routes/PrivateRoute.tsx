bb2da81aa3f27287e27c54bae927a5506ba02128
Jose <josemsilva1984@gmail.com>
2026-04-26 20:23:42 -0300

    ajuste de segurança hash

--- admin/src/routes/PrivateRoute.tsx
+++ admin/src/routes/PrivateRoute.tsx
@@ -1,6 +1,12 @@
+import { Navigate } from "react-router-dom"
 import { ReactNode } from "react"
 
 export default function PrivateRoute({ children }: { children: ReactNode }) {
-  // 🔥 NÃO BLOQUEIA MAIS — login controlado por modal
+  const token = sessionStorage.getItem("token")
+
+  if (!token) {
+    return <Navigate to="/login" replace />
+  }
+
   return <>{children}</>
 }
