28c537770049104aedd7b52db6b21f85421d7227
Jose <josemsilva1984@gmail.com>
2026-04-25 23:10:24 -0300

    ajuste de segurança

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -4,9 +4,11 @@ import authService from '../services/auth.service'
 
 class AuthController {
   login = asyncHandler(async (request: Request, response: Response) => {
-    const { email, senha } = request.body
+    const { email, senha, password } = request.body
 
-    const result = await authService.login(email, senha)
+    const senhaFinal = senha || password
+
+    const result = await authService.login(email, senhaFinal)
 
     return response.json({
       success: true,
