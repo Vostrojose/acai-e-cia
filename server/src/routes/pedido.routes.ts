b9b3538ebdbf80f069e49ec91bf22b2baa677b08
Jose <josemsilva1984@gmail.com>
2026-05-01 03:30:52 -0300

    implementação fiados

--- server/src/routes/pedido.routes.ts
+++ server/src/routes/pedido.routes.ts
@@ -25,7 +25,7 @@ router.get('/', async (req, res) => {
 
     const horas = Number(req.query.horas) || 24 // padrão 24h
     const page = Number(req.query.page) || 1
-    const limit = Number(req.query.limit) || 10
+    const limit = Number(req.query.limit) || 20
 
     const skip = (page - 1) * limit
 
