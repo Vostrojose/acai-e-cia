2ae37ecd22aa4b9c40b1b3af22842b1e349a0f79
Jose <josemsilva1984@gmail.com>
2026-05-03 00:29:45 -0300

    ajustes adicionais

--- admin/src/pages/Adicionais.tsx
+++ admin/src/pages/Adicionais.tsx
@@ -3,6 +3,10 @@ import { useParams, useNavigate } from 'react-router-dom'
 import api from '../services/api'
 import { theme } from '../assets/styles/adminTheme'
 
+const { id } = useParams()
+
+console.log('ID ADICIONAIS:', id)
+
 type Adicional = {
   id: string
   nome: string
