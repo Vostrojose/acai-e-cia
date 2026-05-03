835236e2fd60aa45a52293bf50788d4ae62e2691
Jose <josemsilva1984@gmail.com>
2026-05-02 22:19:39 -0300

    ajustes no produto  Schema

--- server/src/services/produto.service.ts
+++ server/src/services/produto.service.ts
@@ -15,6 +15,24 @@ interface CriarProdutoDTO {
   disponivelDom?: any
 }
 
+import { z } from "zod";
+
+export const criarProdutoSchema = z.object({
+  nome: z.string(),
+  descricao: z.string().optional(),
+  preco: z.number(),
+
+  ativo: z.boolean().optional(),
+
+  /* 🔥 ADICIONAR ISSO */
+  disponivelSeg: z.boolean().optional(),
+  disponivelTer: z.boolean().optional(),
+  disponivelQua: z.boolean().optional(),
+  disponivelQui: z.boolean().optional(),
+  disponivelSex: z.boolean().optional(),
+  disponivelSab: z.boolean().optional(),
+  disponivelDom: z.boolean().optional(),
+});
 class ProdutoService {
 
   async criarProduto(data: CriarProdutoDTO) {
