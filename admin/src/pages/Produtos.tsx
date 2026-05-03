28876ce53d8c7d0009ff8a9609f2fae13e93e45a
Jose <josemsilva1984@gmail.com>
2026-05-02 23:47:40 -0300

    ajustes adicionais

--- admin/src/pages/Produtos.tsx
+++ admin/src/pages/Produtos.tsx
@@ -251,7 +251,7 @@ export default function Produtos() {
               </button>
               <button
                 onClick={() =>
-                  exigirLogin(() => {
+                  exigirReautenticacao(() => {
                     navigate(`/produtos/${p.id}/adicionais`)
                   })
                 }
