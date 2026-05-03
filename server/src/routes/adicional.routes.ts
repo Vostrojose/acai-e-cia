6edff610bd7af99036602265776e9c6def69f6fc
Jose <josemsilva1984@gmail.com>
2026-05-03 01:43:38 -0300

    ajustes adicionais

--- server/src/routes/adicional.routes.ts
+++ server/src/routes/adicional.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { AdicionalController } from '../controllers/adicional.controller'
 
+
 const router = Router()
 const controller = new AdicionalController()
 
 router.post('/', controller.criar)
 router.delete('/:id', controller.remover)
 router.put('/:id', controller.atualizar)
-router.patch('/:id/status', controller.alterarStatus)
+router.patch('/:id', controller.alterarStatus)
 
 export default router
