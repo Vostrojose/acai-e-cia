48b2a1e80db432a33e701eac94f64e496ccfe601
Jose <josemsilva1984@gmail.com>
2026-05-12 22:07:08 -0300

    ajuste produto service

--- server/src/services/produto.service.ts
+++ server/src/services/produto.service.ts
@@ -5,6 +5,14 @@ interface CriarProdutoDTO {
   descricao?: string
   preco: number
   ativo?: boolean
+
+  disponivelDom?: boolean
+  disponivelSeg?: boolean
+  disponivelTer?: boolean
+  disponivelQua?: boolean
+  disponivelQui?: boolean
+  disponivelSex?: boolean
+  disponivelSab?: boolean
 }
 
 class ProdutoService {
@@ -15,37 +23,44 @@ class ProdutoService {
         descricao: data.descricao,
         preco: data.preco,
         ativo: data.ativo ?? true,
+
+        disponivelDom: data.disponivelDom ?? false,
+        disponivelSeg: data.disponivelSeg ?? false,
+        disponivelTer: data.disponivelTer ?? false,
+        disponivelQua: data.disponivelQua ?? false,
+        disponivelQui: data.disponivelQui ?? false,
+        disponivelSex: data.disponivelSex ?? false,
+        disponivelSab: data.disponivelSab ?? false,
       },
     })
   }
 
-async listarProdutos() {
-  const produtos = await prisma.produto.findMany({
-    orderBy: {
-      criadoEm: 'desc',
-    },
-    include: {
-      adicionais: true // 🔥 ESSENCIAL
-    }
-  })
+  async listarProdutos() {
+    const produtos = await prisma.produto.findMany({
+      orderBy: {
+        criadoEm: 'desc',
+      },
+      include: {
+        adicionais: true, // 🔥 ESSENCIAL
+      },
+    })
 
-  return produtos.map((p) => ({
-    ...p,
-    preco: Number(p.preco),
-  }))
-}
+    return produtos.map((p) => ({
+      ...p,
+      preco: Number(p.preco),
+    }))
+  }
   async alterarStatus(id: string, ativo: boolean) {
-  return await prisma.produto.update({
-    where: { id },
-    data: { ativo },
-  })
-}
-async removerProduto(id: string) {
-  return await prisma.produto.delete({
-    where: { id }
-  })
-}
-
+    return await prisma.produto.update({
+      where: { id },
+      data: { ativo },
+    })
+  }
+  async removerProduto(id: string) {
+    return await prisma.produto.delete({
+      where: { id },
+    })
+  }
 }
 
 export default new ProdutoService()
