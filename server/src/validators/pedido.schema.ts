c84d76b7929207d12511be6b3aca822825264a37
Jose <josemsilva1984@gmail.com>
2026-05-25 22:16:38 -0300

    correção server Pedido nome

--- server/src/validators/pedido.schema.ts
+++ server/src/validators/pedido.schema.ts
@@ -1,23 +1,29 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 export const criarPedidoSchema = z.object({
   telefone: z.string().min(10),
-  origem: z.enum(["QR_CODE", "APP", "ADMIN", "BALCAO"]),
+  origem: z.enum(['QR_CODE', 'APP', 'ADMIN', 'BALCAO']),
 
   endereco: z.string().nullable().optional(),
 
   itens: z.array(
     z.object({
       produtoId: z.string().uuid(),
+
+      nome: z.string(),
+
+      preco: z.number(),
+
       quantidade: z.number().min(1),
 
-      // 🔥 CORREÇÃO AQUI (ESSENCIAL)
-      adicionais: z.array(
-        z.object({
-          nome: z.string(),
-          preco: z.number()
-        })
-      ).optional()
-    })
+      adicionais: z
+        .array(
+          z.object({
+            nome: z.string(),
+            preco: z.number(),
+          }),
+        )
+        .optional(),
+    }),
   ),
-});
+})
