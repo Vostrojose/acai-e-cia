233d79eb3bdf9ddd4668729abf99245d8bbb3f47
Jose <josemsilva1984@gmail.com>
2026-06-14 22:56:56 -0300

    feat: android tv heartbeat online

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,13 +31,6 @@ import playerRoutes from './routes/player.routes'
 import path from 'path'
 
 const app = express()
-app.get('/teste-tv', (req, res) => {
-  console.log('TV ACESSOU TESTE')
-
-  return res.json({
-    ok: true,
-  })
-})
 
 /* =================================
    MIDDLEWARES GLOBAIS
