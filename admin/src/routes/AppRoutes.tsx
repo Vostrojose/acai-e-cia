71958f668e6211cb5c72f367235911ebab884714
Jose <josemsilva1984@gmail.com>
2026-06-21 22:07:36 -0300

    telas admin botões nav

--- admin/src/routes/AppRoutes.tsx
+++ admin/src/routes/AppRoutes.tsx
@@ -19,7 +19,6 @@ import Balcao from '../pages/Balcao'
 import TVPlayer from '../pages/TVPlayer'
 import Propagandas from '../pages/Propagandas'
 import Playlists from '../pages/Playlists'
-import PlaylistDetalhe from '../pages/PlaylistDetalhe'
 import TVs from '../pages/TVs'
 import MonitoramentoTV from '../pages/MonitoramentoTV'
 
@@ -172,14 +171,6 @@ export default function AppRoutes() {
         element={
           <PrivateRoute>
             <Playlists />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/playlists/:id"
-        element={
-          <PrivateRoute>
-            <PlaylistDetalhe />
           </PrivateRoute>
         }
       />
