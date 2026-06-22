8739b816b19a749bd73eab291abdda0831b7b106
Jose <josemsilva1984@gmail.com>
2026-06-21 21:53:30 -0300

    telas admin botões nav

--- admin/src/pages/MonitoramentoTV.tsx
+++ admin/src/pages/MonitoramentoTV.tsx
@@ -88,7 +88,7 @@ export default function MonitoramentoTV() {
             <button style={menuButton} onClick={() => navigate('/propagandas')}>
               📢 Propagandas
             </button>
-            <button style={menuButton} onClick={() => navigate('/playlistDetalhe')}>
+            <button style={menuButton} onClick={() => navigate('/playlistdetalhe')}>
               playlistDetalhe
             </button>
 
