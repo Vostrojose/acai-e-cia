71aa56b2e69eb6a8ef46a6126083a9f435a7e7ef
Jose <josemsilva1984@gmail.com>
2026-07-07 20:57:00 -0300

    Log heartbeat

--- server/src/services/tv.service.ts
+++ server/src/services/tv.service.ts
@@ -1,16 +1,27 @@
 import prisma from '../lib/prisma'
 
 class TVService {
-async heartbeat(codigo: string) {
-  return prisma.tV.update({
-    where: {
-      codigo,
-    },
-    data: {
-      ultimaSync: new Date(),
-    },
-  })
-}
+  async heartbeat(codigo: string) {
+    try {
+      console.log('HEARTBEAT:', codigo)
+
+      const tv = await prisma.tV.update({
+        where: {
+          codigo,
+        },
+        data: {
+          ultimaSync: new Date(),
+        },
+      })
+
+      console.log('TV ATUALIZADA:', tv)
+
+      return tv
+    } catch (error) {
+      console.error('ERRO HEARTBEAT:', error)
+      throw error
+    }
+  }
   async listar() {
     return prisma.tV.findMany({
       include: {
@@ -70,32 +81,30 @@ async heartbeat(codigo: string) {
   }
 
   async registrar() {
-  let codigo = ''
+    let codigo = ''
 
-  let existe = true
+    let existe = true
 
-  while (existe) {
-    codigo = `TV-${Math.floor(
-      100000 + Math.random() * 900000,
-    )}`
+    while (existe) {
+      codigo = `TV-${Math.floor(100000 + Math.random() * 900000)}`
 
-    const tvExistente = await prisma.tV.findUnique({
-      where: {
+      const tvExistente = await prisma.tV.findUnique({
+        where: {
+          codigo,
+        },
+      })
+
+      existe = Boolean(tvExistente)
+    }
+
+    return prisma.tV.create({
+      data: {
+        nome: 'TV Não Configurada',
         codigo,
+        ativa: true,
       },
     })
-
-    existe = Boolean(tvExistente)
   }
-
-  return prisma.tV.create({
-    data: {
-      nome: 'TV Não Configurada',
-      codigo,
-      ativa: true,
-    },
-  })
-}
   async status() {
     const tvs = await prisma.tV.findMany({
       include: {
